c8f81c0abd80d67aede21b13e47f4bb35b916c2d
KhyatiDhaval <patelkhyati2009@yahoo.com>
2022-01-08 12:49:26 +0000

    deleted my first code

--- AboutIntellij.java
+++ AboutIntellij.java
@@ -6,7 +6,6 @@ public class AboutIntellij {
 
       System.out.println("Intellij IDEA is an intergrated development environment(IDE)");
       System.out.println(" written in JAVA for developing computer software.");
-      System.out.println(" It also provides intelligent coding assistance for a large variety of other languages such as SQL, HTML, etc.");
       System.out.println(" it is developed by JetBrains.");
       System.out.println(" IntelliJ is a perfect choice for beginners.");
       System.out.println(" And it stood at 3rd place on the list of best source code editors.");
