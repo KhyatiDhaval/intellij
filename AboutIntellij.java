c945972a40e85138b34c6ed60f720f1cf4f421b2
KhyatiDhaval <patelkhyati2009@yahoo.com>
2022-01-08 12:24:36 +0000

    this is my first commit

---
+++ AboutIntellij.java
@@ -0,0 +1,15 @@
+package com.intellij;
+
+public class AboutIntellij {
+
+  public static void main(String[] args)  {
+
+      System.out.println("Intellij IDEA is an intergrated development environment(IDE)");
+      System.out.println(" written in JAVA for developing computer software.");
+      System.out.println(" It also provides intelligent coding assistance for a large variety of other languages such as SQL, HTML, etc.");
+      System.out.println(" it is developed by JetBrains.");
+  }
+
+
+
+}
